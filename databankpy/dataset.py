5548427af8ac21bee0281a0adfe96cff97fb2b52
joshunrau <contact@joshuaunrau.com>
2023-07-05 18:31:27 -0400

    add numpy

--- databankpy/dataset.py
+++ databankpy/dataset.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 import csv
 import sys
 
-from typing import cast, overload, Literal, TypedDict
+from typing import cast, overload, Any, Literal, TypedDict
 
+import numpy as np
+import numpy.typing as npt
 import requests
 
 from databankpy import BASE_URL
@@ -14,7 +16,16 @@ DatasetColumnType = Literal["FLOAT", "INTEGER", "STRING"]
 
 DatasetLicense = Literal["PUBLIC_DOMAIN", "OTHER"]
 
-DatasetColumnData = list[int | float | str]
+DatasetColumnData = (
+    npt.NDArray[np.int32] | npt.NDArray[np.float64] | npt.NDArray[np.str_]
+)
+
+DTYPE_MAP: dict[DatasetColumnType, npt.DTypeLike] = {
+    "FLOAT": np.float64,
+    "INTEGER": np.int32,
+    "STRING": np.str_,
+}
+
 
 class DatasetColumnDefinition(TypedDict):
     description: str
@@ -43,13 +54,14 @@ class Dataset:
         self._name = info["name"]
         self._description = info["description"]
         self._license = info["license"]
-        self._columns = {
-            k: cast(DatasetColumn, {**v, "data": []})
-            for k, v in info["columns"].items()
-        }
-    
+        self._columns = {}
+
+        for key, value in info["columns"].items():
+            self._columns[key] = cast(DatasetColumn, value)
+            self._columns[key]["data"] = np.ndarray(0, dtype=DTYPE_MAP[value["type"]])
+
     def __len__(self) -> int:
-        return len(self.columns[next(iter(self.columns))]['data'])
+        return len(self.columns[next(iter(self.columns))]["data"])
 
     @property
     def name(self) -> str:
@@ -71,17 +83,29 @@ class Dataset:
         """A dictionary mapping column names to metadata and an array of values"""
         return self._columns
 
+    @property
+    def column_names(self) -> npt.NDArray[np.str_]:
+        return np.array(list(self.columns.keys()), dtype=np.str_)
+
     def append_csv(self, filepath: str) -> None:
         """Load the rows from a CSV file and append them to the dataset"""
         with open(filepath, "r", newline="") as f:
             reader = csv.reader(f, delimiter=",", strict=True)
-            column_names = next(reader)
-            for col in self.columns.keys():
-                if col not in column_names:
+
+            csv_columns: dict[str, list[str]] = {k: [] for k in next(reader)}
+            for col in self.column_names:
+                if col not in csv_columns:
                     raise RuntimeError(f"Column '{col}' not found in file: {filepath}")
+
             for row in reader:
-                for i in range(len(row)):
-                    self.columns[column_names[i]]["data"].append(row[i])
+                for i, col in enumerate(csv_columns.keys()):
+                    csv_columns[col].append(row[i])
+
+            for key, value in csv_columns.items():
+                self.columns[key]["data"] = np.append(
+                    self.columns[key]["data"],
+                    np.array(value, dtype=DTYPE_MAP[self.columns[key]["type"]]),
+                )
 
     @overload
     def upload(self, access_token: str) -> None:
